25023e3308b10c19ba1e3514d7313fa14154edf0
priscilafernandes <pris1@outlook.com.br>
2020-02-20 17:26:26 -0300

    validando campos form ok

--- src/app.js
+++ src/app.js
@@ -1,37 +1,28 @@
+const formInput = document.getElementById("form-input");
 const btnSubmit = document.getElementById("btn-submit");
 btnSubmit.addEventListener("click", submit);
 
 function submit(e) {
   e.preventDefault();
-  
-  const name = document.getElementById("name");
-  const lastName = document.getElementById("last-name");
-  const email = document.getElementById("email");
-  // const validateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const validateEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-  const password = document.getElementById("password");
+
+  let name = document.getElementById("name");
+  let lastName = document.getElementById("last-name");
+  let email = document.getElementById("email");
+  const validateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let password = document.getElementById("password");
 
   if (name.value.length <= 0) {
     renderError(name, "First name cannot be empty");
-  } 
-  
-  if (lastName.value.length <= 0) {
+  } else if (lastName.value.length <= 0) {
     renderError(lastName, "Last name cannot be empty");
-  } 
-  
-  if (email.value.length <= 0 || !validateEmail.test(email)) {
+  } else if (email.value.length <= 0 || !validateEmail.test(email.value)) {
     renderError(email, "Looks like this is not an email");
-  }
-  
-  if (password.value.length <= 0) {
+  } else if (password.value.length <= 0) {
     renderError(password, "Password cannot be empty");
-  } 
-  
-  else {
+  } else {
     alert("Submit!");
+    formInput.reset();
   }
-
-  console.log(validateEmail.test(email))
 }
 
 function renderError(el, text) {
